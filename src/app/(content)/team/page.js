dcc387d85a5adced58073bceb46d3ea885759e1e
John Gibbons <jvgiv12@gmail.com>
2026-05-15 12:20:16 -0400

    names added

--- src/app/(content)/team/page.js
+++ src/app/(content)/team/page.js
@@ -5,50 +5,62 @@ const teamMembers = [
   {
     img: "/team/leadprotandfab.png",
     title: "Lead Prototyper and Fabricator",
+    name: "Kevin Barker"
   },
   {
     img: "/team/leadmach.png",
     title: "Lead Machinist and Operator",
+    name: "Brandon Parkinson"
   },
   {
     img: "/team/machandop.png",
     title: "Machinist and Operator",
+    name: "Christopher Herrera"
   },
   {
     img: "/team/caddesign.png",
     title: "CAD Design Specialist",
+    name: "Oisin O'Donnell"
   },
   {
     img: "/team/recorder.png",
     title: "Recorder",
+    name: "Lorenzo Ruzzo"
   },
   {
     img: "/team/cnc.png",
     title: "CNC Specialist/Builder",
+    name: "Jackson Kavanagh"
   },
   {
     img: "/team/designer.png",
     title: "Designer",
+    name: "Brayden Smith"
   },
   {
     img: "/team/scouter.png",
     title: "Scouter",
+    name: "Alina Dorsey"
   },
   {
     img: "/team/progspec.png",
     title: "Programming Specialist",
+    name: "Rylan Kabalkin"
   },
   {
     img: "/team/programmer.png",
     title: "Programmer",
+    name: "Zack Barker"
   },
   {
     img: "/team/marketer.png",
     title: "Marketing Specialist",
+    name: "Aiden Bricker"
   },
   {
     img: "/team/designer2.png",
     title: "Designer",
+    name: "Matthew St. Laurent"
   },
 ];
 
@@ -94,12 +106,13 @@ export default function Team() {
             <div className="member-photo-frame">
               <Image
                 src={member.img}
-                alt={member.title}
+                alt={member.name}
                 width={320}
                 height={320}
                 className="member-photo"
               />
             </div>
+            <h3>{member.name}</h3>
             <p className="member-title">{member.title}</p>
           </article>
         ))}
